ffdea1e613b6262599041ec67b36a81bc8926fdd
Colby Nyce <cnyce@mips.com>
2024-07-17 12:58:00 -0500

    Tweaks to the vscode* scripts

--- vscode_debug.sh
+++ vscode_debug.sh
@@ -2,9 +2,12 @@
 
 mkdir -p debug
 cd debug
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-make clean
-make regress
+rm -f build_debug.log
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .. 2>&1 | tee build_debug.log
+make clean 2>&1 | tee build_debug.log
+make regress 2>&1 | tee build_debug.log
 cd ..
 rm -f compile_commands.json
-ln -s release/compile_commands.json
+ln -s debug/compile_commands.json
+rm -f build.log
+ln -s debug/build_debug.log build.log
--- vscode_release.sh
+++ vscode_release.sh
@@ -2,9 +2,12 @@
 
 mkdir -p release
 cd release
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-make clean
-make regress
+rm -f build_release.log
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .. 2>&1 | tee build_release.log
+make clean 2>&1 | tee build_release.log
+make regress 2>&1 | tee build_release.log
 cd ..
 rm -f compile_commands.json
 ln -s release/compile_commands.json
+rm -f build.log
+ln -s release/build_release.log build.log
