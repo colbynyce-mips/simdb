0a2a1ebfd5e720519ee0df2e93f0c4d66fa9649b
Colby Nyce <iheartridleyman@gmail.com>
2024-07-24 13:05:39 -0500

    Tidy up the SQL tests

--- test/SQLiteConnection/main.cpp
+++ test/SQLiteConnection/main.cpp
@@ -15,21 +15,19 @@ int main()
 
     schema.addTable("Metadata")
         .addColumn("Name", dt::string_t)
-        .addColumn("TheInt", dt::int32_t)
-        .addColumn("TheDouble", dt::double_t)
-        .addColumn("TheIndexedDouble", dt::double_t)->index()
-        .addColumn("TheOtherIndexedDouble", dt::double_t)->indexAgainst({"Name", "TheInt"})
-        .addColumn("SomeString", dt::string_t)->setDefaultValue("foo")
-        .addColumn("OtherString", dt::string_t)->setDefaultValue(std::string("foo"))
-        .addColumn("SomeInt", dt::int32_t)->setDefaultValue(4)
-        .addColumn("SomeDouble", dt::double_t)->setDefaultValue(3.14)
-        .addColumn("SomeBlob", dt::blob_t);
+        .addColumn("SomeInt", dt::int32_t)
+        .addColumn("SomeDouble", dt::double_t)
+        .addColumn("SomeString", dt::string_t)
+        .addColumn("SomeBlob", dt::blob_t)
+        .addColumn("DefaultInt", dt::int32_t)->setDefaultValue(4)
+        .addColumn("DefaultDouble", dt::double_t)->setDefaultValue(3.14)
+        .addColumn("DefaultString", dt::string_t)->setDefaultValue("foo");
 
     simdb::DatabaseManager db_mgr;
     EXPECT_TRUE(db_mgr.createDatabaseFromSchema(schema));
 
     // Create some records and verify the INSERT was successful.
-    int dbid1 = db_mgr.INSERT(SQL_TABLE("Metadata"), SQL_COLUMNS("TheInt", "TheDouble"), SQL_VALUES(777, 3.14));
+    int dbid1 = db_mgr.INSERT(SQL_TABLE("Metadata"), SQL_COLUMNS("SomeInt", "SomeDouble"), SQL_VALUES(777, 3.14));
     EXPECT_NOTEQUAL(dbid1, 0);
     EXPECT_NOTEQUAL(dbid1, -1);
 
