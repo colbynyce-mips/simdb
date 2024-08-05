fd0e3ba85559bfceb6b0e0d33a6d5aba4146e4d5
Colby Nyce <iheartridleyman@gmail.com>
2024-08-05 13:15:48 -0500

    Report all errors to CTest

--- test/SQLiteConnection/main.cpp
+++ test/SQLiteConnection/main.cpp
@@ -844,4 +844,9 @@ int main()
 
     // Verify that we cannot open a database connection for an invalid file
     EXPECT_THROW(simdb::DatabaseManager db_mgr3(__FILE__));
+
+    // This MUST be put at the end of unit test files' main() function.
+    ENSURE_ALL_REACHED(0);
+    REPORT_ERROR;
+    return ERROR_CODE;
 }
