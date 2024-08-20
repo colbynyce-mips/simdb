2dbd143d8277f29700dc710a453c99003a5fa78a
Colby Nyce <iheartridleyman@gmail.com>
2024-08-20 08:35:36 -0500

    Add rapidjson to the cmake find_package list

--- cmake/simdb-config.cmake
+++ cmake/simdb-config.cmake
@@ -2,7 +2,21 @@
 #                     RequiredLibraries                       #
 ###############################################################
 
+# Find RapidJSON
+find_package (RapidJSON 1.1 REQUIRED)
+include_directories (SYSTEM ${RapidJSON_INCLUDE_DIRS})
+message (STATUS "Using RapidJSON CPP ${RapidJSON_VERSION}")
+
 # Find SQLite3
-find_package(SQLite3 3.19 REQUIRED)
+find_package (SQLite3 3.19 REQUIRED)
+include_directories (SYSTEM ${SQLite3_INCLUDE_DIRS})
+message (STATUS "Using SQLite3 ${SQLite3_VERSION}")
+
+# Find zlib
 find_package(ZLIB REQUIRED)
-set(SimDB_LIBS ${SQLite3_LIBRARIES} ZLIB::ZLIB pthread)
+include_directories(SYSTEM ${ZLIB_INCLUDE_DIRS})
+message (STATUS "Using zlib ${ZLIB_VERSION_STRING}")
+
+# Populate the Sparta_LIBS variable with the required libraries for
+# basic Sparta linking
+set (SimDB_LIBS sqlite3 ZLIB::ZLIB pthread)
