b606d2e1a96704a1e66d3b717aa20e78c9695b0d
Colby Nyce <iheartridleyman@gmail.com>
2024-08-25 07:41:01 -0500

    Rename simdb_collections.py to data_retriever.py

--- python/viewer/model/data_retriever.py
+++ python/viewer/model/data_retriever.py
@@ -1,16 +1,6 @@
-#!/usr/bin/python3
+import sqlite3, zlib, struct, json, sys
 
-# This module makes it easy to unpack statistics data that was
-# serialized to the database using SimDB's collection feature.
-#
-# It is provided as a python module to help keep the internal
-# details of this feature hidden from users, or at least not
-# required that users interact directly with the internally-
-# created schema tables to support collection.
-
-import argparse, sqlite3, zlib, struct, json, sys
-
-class Collections:
+class DataRetriever:
     def __init__(self, db_path):
         self._conn = sqlite3.connect(db_path)
         cursor = self.cursor()
@@ -56,7 +46,7 @@ class Collections:
         enums_by_name = {}
         for enum_name,enum_str,enum_blob,int_type in cursor.fetchall():
             if enum_name not in enums_by_name:
-                enums_by_name[enum_name] = Enum(enum_name, int_type)
+                enums_by_name[enum_name] = EnumDef(enum_name, int_type)
 
             enums_by_name[enum_name].AddEntry(enum_str, enum_blob)
 
@@ -153,7 +143,7 @@ class Collections:
         else:
             return time_val
 
-class Enum:
+class EnumDef:
     def __init__(self, name, int_type):
         self._name = name
         self._strings_by_int = {}
@@ -407,65 +397,3 @@ class EnumFormatter(Formatter):
 
     def Format(self, val):
         return self._enum_handler.Format(val)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--db-file', required=True, help='Path to the database file')
-
-    parser.add_argument('--element-path', help='Path of a single element to unpack. If not ' \
-                        'given, all elements will be unpacked.')
-
-    parser.add_argument('--time-range', nargs='+', type=float,
-                        help='Narrow the extracted data to the given time range, given as ' \
-                        '"10 400" (everything between time 10 and time 400) or ' \
-                        '"-1 400" (everything up to time 400) or ' \
-                        '"10 -1" (everything from time 10 onward)')
-
-    parser.add_argument('--show-element-paths', action='store_true', help='List all element paths and exit')
-
-    parser.add_argument('--dump-to-file', action='store_true', help='Dump all unpacked data to file(s) on disk')
-
-    parser.add_argument('--dump-to-stdout', action='store_true', help='Dump all unpacked data to stdout')
-
-    args = parser.parse_args()
-
-    db_path = args.db_file
-    element_path = args.element_path
-    time_range = args.time_range
-    dump_to_file = args.dump_to_file
-    dump_to_stdout = args.dump_to_stdout
-
-    collections = Collections(db_path)
-    
-    if args.show_element_paths:
-        print ('All element paths found in database:')
-        for element_path in collections.GetAllElementPaths():
-            print ('  ' + element_path)
-        sys.exit(0)
-
-    unpacked = {}
-    errmsgs_by_element_path = {}
-    if element_path is None:
-        for element_path in collections.GetAllElementPaths():
-            try:
-                unpacked[element_path] = collections.Unpack(element_path, time_range)
-            except Exception as e:
-                errmsgs_by_element_path[element_path] = str(e)
-    else:
-        try:
-            unpacked[element_path] = collections.Unpack(element_path, time_range)
-        except Exception as e:
-            errmsgs_by_element_path[element_path] = str(e)
-
-    if dump_to_stdout:
-        print (unpacked)
-    elif dump_to_file:
-        for element_path, unpacked in unpacked.items():
-            with open(element_path, 'w') as fout:
-                json.dump(unpacked, fout)
-
-    if len(errmsgs_by_element_path) > 0:
-        print ('Errors occurred while unpacking data from the following elements:')
-        for element_path,errmsg in errmsgs_by_element_path.items():
-            print ('  {}: {}'.format(element_path, errmsg))
