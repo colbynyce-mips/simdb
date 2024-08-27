eaac0e31c0abad9a99c58ec1fc1f91ac6d1266e3
Colby Nyce <iheartridleyman@gmail.com>
2024-08-27 12:00:58 -0500

    Show the struct field values in the ScalarStruct widget

--- python/viewer/gui/widgets/scalar_struct.py
+++ python/viewer/gui/widgets/scalar_struct.py
@@ -5,9 +5,14 @@ class ScalarStruct(wx.Panel):
         super(ScalarStruct, self).__init__(parent)
         self.frame = frame
         self.elem_path = elem_path
+        self.struct_text_elem = wx.StaticText(self, label='Scalar Struct:\n%s' % elem_path)
+        self._field_names = frame.data_retriever.GetDeserializer(elem_path).GetFieldNames()
+
+        font = wx.Font(10, wx.FONTFAMILY_MODERN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+        self.struct_text_elem.SetFont(font)
 
         self.sizer = wx.BoxSizer(wx.VERTICAL)
-        self.sizer.Add(wx.StaticText(self, label='Scalar Struct:\n%s' % elem_path), 0, wx.EXPAND)
+        self.sizer.Add(self.struct_text_elem, 0, wx.EXPAND)
         self.SetSizer(self.sizer)
         self.Layout()
 
@@ -15,4 +20,15 @@ class ScalarStruct(wx.Panel):
         return 'ScalarStruct$' + self.elem_path
 
     def UpdateWidgetData(self):
-        pass
+        widget_renderer = self.frame.widget_renderer
+        tick = widget_renderer.tick
+        queue_data = self.frame.data_retriever.Unpack(self.elem_path, (tick,tick))
+
+        field_max_len = max([len(field_name) for field_name in self._field_names])
+        struct_str = []
+        for field_name in self._field_names:
+            field_val = queue_data['DataVals'][0][field_name]
+            field_name = field_name.ljust(field_max_len)
+            struct_str.append('%s: %s' % (field_name, field_val))
+
+        self.struct_text_elem.SetLabel('\n'.join(struct_str))
