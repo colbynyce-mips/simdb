857007025a2f512c549c17b02422965bec7679a2
Colby Nyce <iheartridleyman@gmail.com>
2024-10-27 15:33:46 -0500

    Fill in more impl for the Scheduling Lines feature

---
+++ python/viewer/gui/widgets/grid.py
@@ -0,0 +1,133 @@
+import wx, wx.grid
+
+class Grid(wx.grid.Grid):
+    def __init__(self, parent, frame, rows, cols, cell_font=None, label_font=None, cell_selection_allowed=True, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.frame = frame
+        self.renderer = GridCellRenderer(rows, cols, cell_font)
+
+        if label_font:
+            self.SetLabelFont(label_font)
+
+        self.CreateGrid(rows, cols)
+        self.EnableEditing(False)
+        self.DeselectCell(0,0)
+
+        if not cell_selection_allowed:
+            self.Bind(wx.grid.EVT_GRID_SELECT_CELL, lambda evt: evt.Veto())
+
+        self.SetDefaultRenderer(self.renderer)
+
+    def SetCellValue(self, row, col, value, immediate_refresh=False):
+        self.renderer.SetCellValue(row, col, value)
+        if immediate_refresh:
+            self.Refresh()
+            self.AutoSize()
+
+    def GetCellValue(self, row, col):
+        return self.renderer.GetCellValue(row, col)
+    
+    def SetBackgroundColour(self, row, col, color, immediate_refresh=False):
+        self.renderer.SetBackgroundColour(row, col, color)
+        if immediate_refresh:
+            self.Refresh()
+            self.AutoSize()
+
+    def GetBackgroundColour(self, row, col):
+        return self.renderer.GetBackgroundColour(row, col)
+
+    def SetCellBorder(self, row, col, border_width=1, border_side=wx.ALL, immediate_refresh=False):
+        self.renderer.SetBorder(row, col, border_width, border_side)
+        if immediate_refresh:
+            self.Refresh()
+            self.AutoSize()
+
+    def ClearGrid(self):
+        for row in range(self.GetNumberRows()):
+            for col in range(self.GetNumberCols()):
+                self.SetCellValue(row, col, '')
+                self.SetBackgroundColour(row, col, (255, 255, 255))
+                self.SetCellBorder(row, col, 0)
+
+        self.Refresh()
+        self.AutoSize()
+
+class GridCellRenderer(wx.grid.GridCellRenderer):
+    def __init__(self, rows, cols, font=None):
+        super().__init__()
+        self.cells = [[GridCell(font) for _ in range(cols)] for _ in range(rows)]
+
+    def SetCellValue(self, row, col, value):
+        self.cells[row][col].SetText(value)
+
+    def GetCellValue(self, row, col):
+        return self.cells[row][col].GetText()
+
+    def SetBackgroundColour(self, row, col, color):
+        self.cells[row][col].SetBackgroundColour(color)
+
+    def GetBackgroundColour(self, row, col):
+        return self.cells[row][col].GetBackgroundColour()
+
+    def SetBorder(self, row, col, border_width, border_side):
+        self.cells[row][col].SetBorder(border_width, border_side)
+
+    def Draw(self, grid, attr, dc, rect, row, col, isSelected):
+        self.cells[row][col].Draw(grid, dc, rect)
+
+    def GetBestSize(self, grid, attr, dc, row, col):
+        return self.cells[row][col].GetBestSize(grid, attr, dc, row, col)
+
+class GridCell:
+    def __init__(self, font=None):
+        self.text = ''
+        self.font = font if font else wx.Font(12, wx.FONTFAMILY_MODERN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+        self.background_colour = (255, 255, 255)
+        self.border_width = 0
+        self.border_side = wx.ALL
+
+    def SetText(self, text):
+        self.text = text
+
+    def GetText(self):
+        return self.text
+    
+    def SetBackgroundColour(self, color):
+        self.background_colour = color
+
+    def GetBackgroundColour(self):
+        return self.background_colour
+    
+    def SetBorder(self, border_width, border_side):
+        self.border_width = border_width
+        self.border_side = border_side
+
+    def Draw(self, grid, dc, rect):
+        dc.SetBrush(wx.Brush(self.background_colour))
+        dc.SetPen(wx.Pen(wx.TRANSPARENT_PEN))
+        dc.DrawRectangle(rect)
+
+        if self.text:
+            dc.SetFont(self.font)
+            dc.DrawLabel(self.text, rect, wx.ALIGN_CENTER)
+
+        if self.border_width:
+            dc.SetPen(wx.Pen(wx.BLACK, self.border_width))
+            if self.border_side & wx.TOP:
+                dc.DrawLine(rect.GetLeft(), rect.GetTop(), rect.GetRight(), rect.GetTop())
+            if self.border_side & wx.BOTTOM:
+                dc.DrawLine(rect.GetLeft(), rect.GetBottom(), rect.GetRight(), rect.GetBottom())
+            if self.border_side & wx.LEFT:
+                dc.DrawLine(rect.GetLeft(), rect.GetTop(), rect.GetLeft(), rect.GetBottom())
+            if self.border_side & wx.RIGHT:
+                dc.DrawLine(rect.GetRight(), rect.GetTop(), rect.GetRight(), rect.GetBottom())
+
+    def GetBestSize(self, grid, attr, dc, row, col):
+        if not self.text:
+            return (dc.GetTextExtent(' ')[0], 5)
+
+        dc.SetFont(self.font)
+        w,h = dc.GetTextExtent(self.text)
+        w += 2
+        h += 2
+        return wx.Size(w, h)
--- python/viewer/gui/widgets/scheduling_lines.py
+++ python/viewer/gui/widgets/scheduling_lines.py
@@ -1,5 +1,6 @@
-import wx, copy
+import wx, copy, os
 from viewer.gui.view_settings import DirtyReasons
+from viewer.gui.widgets.grid import Grid
 
 class SchedulingLinesWidget(wx.Panel):
     def __init__(self, parent, frame):
@@ -9,25 +10,17 @@ class SchedulingLinesWidget(wx.Panel):
         self.num_ticks_before = 5
         self.num_ticks_after = 50
         self.show_detailed_queue_packets = True
-
-        self.info = wx.StaticText(self, label='Drag queues from the NavTree to create scheduling lines.', size=(600,18))
-        self.info.SetFont(wx.Font(14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
-
-        vsizer = wx.BoxSizer(wx.VERTICAL)
-        vsizer.AddStretchSpacer()
-        vsizer.Add(self.info, 1, wx.ALL | wx.CENTER | wx.EXPAND, 5)
-        vsizer.AddStretchSpacer()
-
-        hsizer = wx.BoxSizer(wx.HORIZONTAL)
-        hsizer.AddStretchSpacer()
-        hsizer.Add(vsizer, 0, wx.CENTER | wx.EXPAND)
-        hsizer.AddStretchSpacer()
-
-        self.SetSizer(hsizer)
-        self.SetBackgroundColour('light gray')
-
+        self.grid = None
+        self.info = None
         self.gear_btn = None
-        self.Layout()
+
+        cursor = frame.db.cursor()
+        cmd = 'SELECT CollectionID,MaxSize FROM QueueMaxSizes'
+
+        cursor.execute(cmd)
+        self.queue_max_sizes_by_collection_id = {}
+        for collection_id,max_size in cursor.fetchall():
+            self.queue_max_sizes_by_collection_id[collection_id] = max_size
 
     def GetWidgetCreationString(self):
         return 'Scheduling Lines'
@@ -51,7 +44,7 @@ class SchedulingLinesWidget(wx.Panel):
 
     def AddElement(self, elem_path):
         self.element_paths.append(elem_path)
-        self.__UpdateInfoText()
+        self.__Refresh()
 
         if not self.gear_btn:
             self.gear_btn = wx.BitmapButton(self, bitmap=self.frame.CreateResourceBitmap('gear.png'), pos=(5,5))
@@ -61,7 +54,10 @@ class SchedulingLinesWidget(wx.Panel):
         self.frame.view_settings.SetDirty(reason=DirtyReasons.SchedulingLinesWidgetChanged)
 
     def UpdateWidgetData(self):
-        pass
+        if not self.grid and not self.info and not self.gear_btn:
+            return
+
+        self.__Refresh()
 
     def GetCurrentViewSettings(self):
         settings = {}
@@ -88,25 +84,199 @@ class SchedulingLinesWidget(wx.Panel):
         self.num_ticks_after = settings['num_ticks_after']
         self.show_detailed_queue_packets = settings['show_detailed_queue_packets']
 
-        self.__UpdateInfoText()
+        self.__Refresh()
         self.frame.view_settings.SetDirty(reason=DirtyReasons.SchedulingLinesWidgetChanged)
 
-    def __UpdateInfoText(self):
+    def __Refresh(self):
         if len(self.element_paths):
-            self.info.SetLabel('\n'.join(self.element_paths))
+            if self.info:
+                self.info.Hide()
+
             self.SetBackgroundColour('white')
+            self.__RegenerateSchedulingLinesGrid()
 
             if not self.gear_btn:
                 self.gear_btn = wx.BitmapButton(self, bitmap=self.frame.CreateResourceBitmap('gear.png'), pos=(5,5))
                 self.gear_btn.Bind(wx.EVT_BUTTON, self.__EditWidget)
                 self.gear_btn.SetToolTip('Edit widget settings')
         else:
-            if self.gear_btn:
-                self.gear_btn.Destroy()
-                self.gear_btn = None
+            self.__ShowUsageInfo()
 
-            self.info.SetLabel('Drag queues from the NavTree to create scheduling lines.')
-            self.SetBackgroundColour('light gray')
+    def __ShowUsageInfo(self):
+        if self.gear_btn:
+            self.gear_btn.Destroy()
+            self.gear_btn = None
+
+        if self.info:
+            self.info.Destroy()
+            self.info = None
+
+        if self.grid:
+            self.grid.Destroy()
+            self.grid = None
+
+        if self.GetSizer():
+            self.GetSizer().Clear()
+
+        self.SetBackgroundColour('light gray')
+
+        self.info = wx.StaticText(self, label='Drag queues from the NavTree to create scheduling lines.', size=(600,18))
+        self.info.SetFont(wx.Font(14, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL))
+
+        vsizer = wx.BoxSizer(wx.VERTICAL)
+        vsizer.AddStretchSpacer()
+        vsizer.Add(self.info, 1, wx.ALL | wx.CENTER | wx.EXPAND, 5)
+        vsizer.AddStretchSpacer()
+
+        hsizer = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer.AddStretchSpacer()
+        hsizer.Add(vsizer, 0, wx.CENTER | wx.EXPAND)
+        hsizer.AddStretchSpacer()
+
+        self.SetSizer(hsizer)
+        self.Layout()
+
+    def __RegenerateSchedulingLinesGrid(self):
+        if self.grid:
+            self.grid.Destroy()
+            self.grid = None
+
+        if self.info:
+            self.info.Destroy()
+            self.info = None
+
+        if self.gear_btn:
+            self.gear_btn.Destroy()
+            self.gear_btn = None
+
+        sizer = self.GetSizer()
+        if sizer:
+            sizer.Clear()
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        # The number of rows can be calculated as:
+        #  1. Go through each element path we are tracking (each is a queue)
+        #     a. For each element path, get the number of bins in each queue (A)
+        #     b. For each element path, note the maximum number of elements seen in the simulation (B)
+        #     c. If A>B, then the number of rows is B+1. Otherwise, the number of rows is A. (C)
+        #  >>> The required number of rows is the sum of all (C) values in the (1) loop.
+
+        num_rows = 0
+        for elem_path in self.element_paths:
+            collection_id = self.frame.simhier.GetCollectionID(elem_path)
+            num_bins = self.frame.simhier.GetCapacityByCollectionID(collection_id) # (A)
+            max_size = self.queue_max_sizes_by_collection_id[collection_id]        # (B)
+            elem_num_rows = max_size + 1 if max_size < num_bins else num_bins      # (C)
+            num_rows += elem_num_rows
+
+        # The number of columns can be calculated as:
+        #  1. Start with the sum of self.num_ticks_before and self.num_ticks_after (A)
+        #  2. Add 1 to (A) to account for the element paths column (captions)
+        #  3. If self.show_detailed_queue_packets is True, add 3 to (A) to account for:
+        #     a. A column to add a separator between the summary and detailed sections
+        #     b. A column to duplicate the element paths column (captions)
+        #     c. A column to show the stringified packet data e.g. "IntVal(4) DoubleVal(3.14)"
+
+        num_cols = self.num_ticks_before + self.num_ticks_after + 1
+        if self.show_detailed_queue_packets:
+            num_cols += 3
+
+        # Create 10-point monospace font for the grid labels and cells
+        font = wx.Font(10, wx.FONTFAMILY_MODERN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL)
+
+        self.grid = Grid(self, self.frame, num_rows, num_cols, cell_font=font, label_font=font, cell_selection_allowed=False)
+        self.gear_btn = wx.BitmapButton(self, bitmap=self.frame.CreateResourceBitmap('gear.png'))
+        self.gear_btn.Bind(wx.EVT_BUTTON, self.__EditWidget)
+
+        current_tick = self.frame.widget_renderer.tick
+        col_labels = []
+        for col in range(1, self.num_ticks_before + self.num_ticks_after + 1):
+            tick = current_tick - self.num_ticks_before + col - 1
+            self.grid.SetColLabelValue(col, str(tick))
+            col_labels.append(str(tick))
+
+        # Use a DC to get the length of the longest col label
+        dc = wx.ScreenDC()
+        dc.SetFont(self.grid.GetLabelFont())
+        max_col_label_len = max([dc.GetTextExtent(col_label)[0] for col_label in col_labels])
+        self.grid.SetColLabelSize(max_col_label_len + 4)
+
+        self.grid.SetColLabelValue(0, '')
+        self.grid.SetColLabelTextOrientation(wx.VERTICAL)
+        self.grid.HideRowLabels()
+
+        sizer.Add(self.gear_btn, 0, wx.ALL, 5)
+        sizer.Add(self.grid, 0, wx.TOP | wx.EXPAND, 5)
+        self.SetSizer(sizer)
+
+        self.grid.ClearGrid()
+
+        self.__SetElementCaptions(0)
+        if self.show_detailed_queue_packets:
+            self.__SetElementCaptions(self.num_ticks_before + self.num_ticks_after + 2)
+
+            # Clear the column labels for the detailed queue packets section
+            for i in range(self.num_ticks_before + self.num_ticks_after + 1, self.grid.GetNumberCols()):
+                self.grid.SetColLabelValue(i, '')
+
+        self.grid.AutoSize()
+        self.Layout()
+
+    def __SetElementCaptions(self, col):
+        captions = []
+        for elem_path in self.element_paths:
+            captions.extend(self.__GetCaptionsForElement(elem_path))
+
+        max_num_chars = max([len(caption) for caption in captions])
+        row_offset = 0
+        for elem_path in self.element_paths:
+            row_offset += self.__SetCaptionsForElement(elem_path, row_offset, col, max_num_chars)
+
+    def __SetCaptionsForElement(self, elem_path, row_offset, col, max_num_chars):
+        collection_id = self.frame.simhier.GetCollectionID(elem_path)
+        num_bins = self.frame.simhier.GetCapacityByCollectionID(collection_id)
+        max_size = self.queue_max_sizes_by_collection_id[collection_id]
+
+        if max_size < num_bins:
+            caption = '{}[{}-{}]'.format(elem_path.split('.')[-1], max_size-1, num_bins-1)
+            caption += ' '*(max_num_chars - len(caption))
+            self.grid.SetCellValue(row_offset, col, caption)
+
+            for i in range(1, max_size):
+                bin_idx = max_size - i - 1
+                caption = '{}[{}]'.format(elem_path.split('.')[-1], bin_idx)
+                caption += ' '*(max_num_chars - len(caption))
+                self.grid.SetCellValue(row_offset + i, col, caption)
+
+            return max_size + 1
+        else:
+            for i in range(num_bins):
+                bin_idx = num_bins - i - 1
+                caption = '{}[{}]'.format(elem_path.split('.')[-1], bin_idx)
+                caption += ' '*(max_num_chars - len(caption))
+                self.grid.SetCellValue(row_offset + i, col, caption)
+
+            return num_bins
+
+    def __GetCaptionsForElement(self, elem_path):
+        collection_id = self.frame.simhier.GetCollectionID(elem_path)
+        num_bins = self.frame.simhier.GetCapacityByCollectionID(collection_id)
+        max_size = self.queue_max_sizes_by_collection_id[collection_id]
+
+        captions = []
+        if max_size < num_bins:
+            captions.append('{}[{}-{}]'.format(elem_path.split('.')[-1], max_size-1, num_bins-1))
+
+            for i in range(1, max_size):
+                bin_idx = max_size - i - 1
+                captions.append('{}[{}]'.format(elem_path.split('.')[-1], bin_idx))
+        else:
+            for i in range(num_bins):
+                bin_idx = num_bins - i - 1
+                captions.append('{}[{}]'.format(elem_path.split('.')[-1], bin_idx))
+
+        return captions
 
     def __EditWidget(self, evt):
         dlg = SchedulingLinesCustomizationDialog(self, self.element_paths, self.num_ticks_before, self.num_ticks_after, self.show_detailed_queue_packets)
